1461b04d3af34c34501c0cf94c0b6ce1367f20e2
Aaron Alofa <aaronalofa@hotmail.fr>
2025-03-19 01:05:01 -0400

    fix

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const MongoDBClient = require('./mongoDBInterface.js').default;
 const mongo = MongoDBClient.getInstance();
-const Util = require('./util.js');
-const u = new Util();
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -11,22 +9,30 @@ const port = 3000;
 app.use(cors({ origin: true }));
 app.use(express.json());
 
+(async () => {
+    try {
+        await mongo.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        process.exit(1);
+    }
+})();
+
 app.get('/', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(203).send('Salut, bienvenue sur le serveur de word surgery!');
 });
 
 app.get('/leaderboard', async (req, res) => {
-    await mongo.connect();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send(await mongo.getLeaderboard());
     console.log("Leaderboard was succesfully fetched.")
-    await mongo.closeConnection();
 });
 
 app.post('/newEntry', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    console.log('Received request body:', req.body); // Add logging to debug the request payload
+    console.log('Received new entry :', req.body);
 
     if (typeof req.body !== 'object' || req.body === null) {
         return res.status(400).send({ message: 'Invalid JSON object' });
@@ -38,12 +44,16 @@ app.post('/newEntry', async (req, res) => {
         return res.status(418).send({ message: 'Missing required fields' });
     }
 
-    await mongo.connect();
     await mongo.addLeaderboardEntry(req.body);
     res.status(202).send({ message: 'New entry added successfully', entry: req.body });
-    await mongo.closeConnection();
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Serveur démarré sur http://localhost:${port} or http://${u.getLocalIp().en0}:${port}`);
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
+
+process.on('SIGINT', async () => {
+    console.log('Closing MongoDB connection...');
+    await mongo.closeConnection();
+    process.exit(0);
 });
